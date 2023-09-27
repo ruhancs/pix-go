f355adfc8869ce59306e738298ec7a8455062991
ruhancs <ruhan_cs@hotmail.com>
2023-09-27 19:38:24 -0300

    feat: :sparkles: adicionado entidades account e bank

---
+++ internal/domain/entity/account.go
@@ -0,0 +1,40 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Account struct {
+	Base `valid:"required"`
+	OwnerName string `json:"owner_name" valid:"notnull"`
+	Bank *Bank `valid:"-"`
+	Number string `json:"number" valid:"notnull"`
+}
+
+func NewAccount(ownerName, number string, bank *Bank) (*Account,error) {
+	account := &Account{
+		OwnerName: ownerName,
+		Number: number,
+		Bank: bank,
+	}
+	account.ID = uuid.NewV4().String()
+	account.CreatedAt = time.Now()
+
+	if err := account.Validate(); err != nil {
+		return nil,err
+	}
+
+	return account,nil
+}
+
+func (b *Account) Validate() error {
+	_, err := govalidator.ValidateStruct(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/domain/entity/bank.go
@@ -0,0 +1,37 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Bank struct {
+	Base `valid:"required"`
+	Code string `json:"code" valid:"notnull"`
+	Name string `json:"name" valid:"notnull"`
+}
+
+func NewBank(code, name string) (*Bank, error) {
+	bank := &Bank{}
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+	bank.Code = code
+	bank.Name = name
+
+	if err := bank.Validate(); err != nil {
+		return nil, err
+	}
+
+	return bank, nil
+}
+
+func (b *Bank) Validate() error {
+	_, err := govalidator.ValidateStruct(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
